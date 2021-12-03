22f420cebf1b57b5d31ea6d5bf3f7cec907980fa
sonali jain <kcjain1970@gmail.com>
2021-12-03 23:40:57 +0530

    2nd commit

--- lib/GoogleSignIn.dart
+++ lib/GoogleSignIn.dart
@@ -6,17 +6,26 @@ class  GoogleSignInProvider extends ChangeNotifier{
   GoogleSignInAccount? _user;
   GoogleSignInAccount get user=> _user!;
   Future googleLogin() async{
-    final google_user=await google_sign_in.signIn();
-    if(google_user==null) return;
-    _user=google_user;
-    final google_auth=await google_user.authentication;
-    final credential=GoogleAuthProvider.credential(
-      accessToken:google_auth.accessToken ,
-      idToken: google_auth.idToken,
-    );
-    await FirebaseAuth.instance.signInWithCredential(credential);
+    try {
+      final google_user = await google_sign_in.signIn();
+      if (google_user == null) return;
+      _user = google_user;
+      final google_auth = await google_user.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: google_auth.accessToken,
+        idToken: google_auth.idToken,
+      );
+      await FirebaseAuth.instance.signInWithCredential(credential);
+    }
+    catch(e)
+    {
+      print(e.toString());
+    }
     //update UI
     notifyListeners();
   }
-
+  Future logout() async{
+    await google_sign_in.disconnect();
+    FirebaseAuth.instance.signOut();
+  }
 }
--- lib/LoggedInWidget.dart
+++ lib/LoggedInWidget.dart
@@ -1,7 +1,9 @@
+import 'package:assgnmnt5adv/FirebaseDemo.dart';
+import 'package:assgnmnt5adv/GoogleSignIn.dart';
 import 'package:flutter/material.dart';
-
-
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 class LoggedInWidget extends StatefulWidget {
   @override
   _State createState() => new _State();
@@ -10,15 +12,38 @@ class LoggedInWidget extends StatefulWidget {
 class _State extends State<LoggedInWidget> {
   @override
   Widget build(BuildContext context) {
+    final   user= FirebaseAuth.instance.currentUser;
     return Scaffold(
       appBar:  AppBar(
         title: const Text('LoggedInWidget'),
+        actions: [
+          TextButton(
+              onPressed: (){
+                final provider=Provider.of<GoogleSignInProvider>(context,
+                    listen:false);
+                provider.logout();
+              },
+              child: Text("Logout",
+        ],
       ),
-      body:  Container(
-        padding:  EdgeInsets.all(32.0),
-        child:  Column(
-          children: const <Widget>[
-             Text('LoggedIn')
+      body:  Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  <Widget>[
+             Text('Profile'),
+            SizedBox(height: 20,),
+            CircleAvatar(
+              radius: 40,
+              backgroundImage: NetworkImage(user!.photoURL!),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              'Name:' +user.displayName!,
+            ),
+            SizedBox(height: 10,),
+            Text(
+              'Email:' +user.email!,
+            ),
           ],
         ),
       ),
